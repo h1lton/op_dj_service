c860bf21ea1d452490a17d7b5316b2a90f46d3be
hilton <uzoversergey@gmail.com>
2023-09-11 08:14:47 +0500

    optimized ORM query in SubscriptionView

--- service/service/settings.py
+++ service/service/settings.py
@@ -89,3 +89,16 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGGING = {
+    'version': 1,
+    'handlers': {
+        'console': {'class': 'logging.StreamHandler'}
+    },
+    'loggers': {
+        'django.db.backends': {
+            'handlers': ['console'],
+            'level': 'DEBUG'
+        }
+    }
+}
--- service/services/views.py
+++ service/services/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render
+from django.db.models import Prefetch
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
-from services.models import Subscription
+from services.models import *
 from services.serializers import SubscriptionSerializer
 
 
 class SubscriptionView(ReadOnlyModelViewSet):
-    queryset = Subscription.objects.all()
+    queryset = Subscription.objects.all().prefetch_related(
+        Prefetch('client', queryset=Client.objects.all().select_related('user').only('company_name', 'user__email'))
+    )
     serializer_class = SubscriptionSerializer
