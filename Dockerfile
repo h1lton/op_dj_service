c6dade1e2a49fbc9bc9e39ba6a8e61d4da64c4fe
hilton <uzoversergey@gmail.com>
2023-09-11 06:21:37 +0500

    changed db to postgres

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,14 @@
-FROM python:3.9-alpine3.16
+FROM python:3.9-alpine3.18
 
 COPY requirements.txt /temp/requirements.txt
 
+RUN apk add postgresql-client build-base postgresql-dev
+
 RUN pip install -r /temp/requirements.txt
 
+EXPOSE 8000
 RUN adduser --disabled-password service-user
-
 USER service-user
+
 COPY service /service
 WORKDIR /service
-EXPOSE 8000
--- service/service/settings.py
+++ service/service/settings.py
@@ -50,8 +50,11 @@ WSGI_APPLICATION = 'service.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': config('DB_HOST'),
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASS'),
     }
 }
 
