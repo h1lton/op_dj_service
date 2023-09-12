8c326a9f6099ea534fd7941f0960becaf4b34838
hilton <uzoversergey@gmail.com>
2023-09-12 18:30:31 +0500

    Added "comment" field to "subs" model, celery task "set_comment". Edited the "set_price" task.

--- service/services/models.py
+++ service/services/models.py
@@ -2,7 +2,7 @@ from django.core.validators import MaxValueValidator
 from django.db import models
 
 from clients.models import Client
-from services.tasks import set_price
+from services.tasks import *
 
 
 class Service(models.Model):
@@ -17,6 +17,7 @@ class Service(models.Model):
         if self.full_price != self.__full_price:
             for sub in self.subscriptions.all():
                 set_price.delay(sub.id)
+                set_comment.delay(sub.id)
             self.__full_price = self.full_price
         return super().save(*args, **kwargs)
 
@@ -39,6 +40,7 @@ class Plan(models.Model):
         if self.discount_percent != self.__discount_percent:
             for sub in self.subscriptions.all():
                 set_price.delay(sub.id)
+                set_comment.delay(sub.id)
             self.__discount_percent = self.discount_percent
         return super().save(*args, **kwargs)
 
@@ -48,3 +50,4 @@ class Subscription(models.Model):
     service = models.ForeignKey(Service, related_name='subscriptions', on_delete=models.PROTECT)
     plan = models.ForeignKey(Plan, related_name='subscriptions', on_delete=models.PROTECT)
     price = models.PositiveIntegerField(default=0)
+    comment = models.CharField(max_length=50, default='')
